32bb117d3e03b4f3048a974f4ae0b7941fe0b6e1
Shawn Gaetano <sgaetano@syr.edu>
2019-04-11 16:53:23 -0400

    Started lab

--- app.js
+++ app.js
@@ -7,4 +7,57 @@ app.config(function($sceDelegateProvider) {
     ]);
 });
 
-const key = '';
+const key = 'AIzaSyDxmtiD7-a4qjC370LGytXEWJ8U2qJN2OY';
+
+
+app.controller('searchCtrl', function($scope, $http) {
+    $scope.search_query = '';
+
+    $scope.search_videos = function() {
+        $http({
+            url:'https://www.googleapis.com/youtube/v3/search',
+            method:'GET',
+            params: {
+                key: key,
+                part:'snippet',
+                maxResults:8,
+                q: $scope.search_query + ' dog', 
+                type:'video'
+            }
+        }).then(function(response) {
+            console.log(response.data.items);
+    
+            $scope.videos = response.data.items;
+        }, function(error){
+            console.log(error);
+        });
+
+    }
+});
+
+
+// playlist in playlists
+// scope.corgi
+// scope.husky
+
+app.controller('videoCtrl', function($scope, $http) {
+    $scope.videos = []; 
+
+    $http({
+        url:'https://www.googleapis.com/youtube/v3/search',
+        method:'GET',
+        params: {
+            key: key,
+            part:'snippet',
+            maxResults:8,
+            q:'dachshund', 
+            type:'video'
+        }
+    }).then(function(response) {
+        console.log(response.data.items);
+
+        $scope.videos = response.data.items;
+    }, function(error){
+        console.log(error);
+    });
+}); 
